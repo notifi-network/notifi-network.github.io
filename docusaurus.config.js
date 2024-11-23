dfc551c24d6f5d5d0fb2bb5aa78f3a693994909a
Nimesh Amin <nimesh@notifi.network>
2024-11-22 23:23:01 -0800

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://notifi-network.github.io',
+  url: 'https://docs.notifi.network',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
